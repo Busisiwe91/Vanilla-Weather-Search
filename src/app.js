da8bfeb7344da4dbfee0bc617fd0b01daa2803e2
Busisiwe91 <busisiwemohloai91@gmail.com>
2023-06-11 13:54:19 +0200

    added a search engine

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let day = date.getDay();
+
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -20,7 +37,20 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "t704280baa3dd7ffc473of4f1b484aa4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${aipKey}$units=metric`;
+function search(city) {
+  let apiKey = "t704280baa3dd7ffc473of4f1b484aa4";
+  let city = "Paris";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${aipKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("city-input");
+  search(cityInputElement.value);
+}
+
+search("Paris");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
